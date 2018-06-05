9cf2086e3c46e912333d2443dee838a8951cc7b6
sbi <szymon.bizon@hicron.com>
2018-06-05 12:20:56 +0200

    Initial commit

---
+++ src/main/java/com/gmail/spbisoft/demorelationaldbini/DemorelationaldbiniApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.spbisoft.demorelationaldbini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorelationaldbiniApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemorelationaldbiniApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gmail/spbisoft/demorelationaldbini/DemorelationaldbiniApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.spbisoft.demorelationaldbini;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemorelationaldbiniApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
